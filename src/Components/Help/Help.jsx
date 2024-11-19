8e1e34726fc3393f8066c5db3a56cf77af6dd903
sadman sakib <sadmansakib1075@gmail.com>
2024-11-19 23:31:35 +0600

    added how to work

--- src/Components/Help/Help.jsx
+++ src/Components/Help/Help.jsx
@@ -1,9 +1,11 @@
+import HowToWorks from "../HowToWorks/HowToWorks";
 
 
 const Help = () => {
     return (
         <div className='ml-4 mr-4 md:ml-40 md:mr-40'>
-           this is help page 
+            <HowToWorks></HowToWorks>
+
         </div>
     );
 };
